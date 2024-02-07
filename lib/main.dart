3f5e7901f49925e0be704bf2ab9626aa2ba2990c
Anant Dubey <anantkumar312@gmail.com>
2024-02-07 12:54:46 +0530

    feat: add add element button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:critiq/screens/book.dart';
-import 'package:critiq/screens/movie.dart';
-
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'package:critiq/screens/home.dart';
 
 var kBookLightColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.light,
@@ -20,8 +18,6 @@ class ModeController extends GetxController {
   Rx<bool> switchBool = false.obs;
 }
 
-final ModeController mc = Get.put(ModeController());
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -29,6 +25,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ModeController mc = Get.put(ModeController());
+
     return Obx(
       () => MaterialApp(
         title: 'Critiq',
@@ -38,68 +36,8 @@ class MyApp extends StatelessWidget {
               : kBookLightColorScheme,
           useMaterial3: true,
         ),
-        home: const HomePage(),
+        home: const HomeScreen(),
       ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final MaterialStateProperty<Icon?> thumbIcon =
-        MaterialStateProperty.resolveWith<Icon?>(
-      (Set<MaterialState> states) {
-        if (states.contains(MaterialState.selected)) {
-          return const Icon(Icons.movie);
-        }
-        return const Icon(
-          Icons.book,
-          color: Colors.black,
-        );
-      },
-    );
-    final MaterialStateProperty<Color?> outlineColor =
-        MaterialStateProperty.resolveWith<Color?>(
-      (final Set<MaterialState> states) {
-        if (states.contains(MaterialState.selected)) {
-          return null;
-        }
-
-        return Colors.blue;
-      },
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Critiq', style: GoogleFonts.getFont('Pacifico')),
-        actions: <Widget>[
-          Obx(
-            () => Switch(
-              thumbIcon: thumbIcon,
-              inactiveTrackColor: Colors.blue,
-              inactiveThumbColor: Colors.white,
-              value: mc.switchBool.value,
-              trackOutlineColor: outlineColor,
-              onChanged: (bool value) {
-                if (value) {
-                  mc.switchBool.value = true;
-                } else {
-                  mc.switchBool.value = false;
-                }
-              },
-            ),
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: mc.switchBool.value ? const MovieScreen() : const BookScreen(),
     );
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:critiq/main.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ModeController mc = Get.put(ModeController());
+
+    final MaterialStateProperty<Icon?> thumbIcon =
+        MaterialStateProperty.resolveWith<Icon?>(
+      (Set<MaterialState> states) {
+        if (states.contains(MaterialState.selected)) {
+          return const Icon(Icons.movie);
+        }
+        return const Icon(
+          Icons.book,
+          color: Colors.black,
+        );
+      },
+    );
+    final MaterialStateProperty<Color?> outlineColor =
+        MaterialStateProperty.resolveWith<Color?>(
+      (final Set<MaterialState> states) {
+        if (states.contains(MaterialState.selected)) {
+          return null;
+        }
+
+        return Colors.blue;
+      },
+    );
+
+    return Obx(
+      () => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('Critiq', style: GoogleFonts.getFont('Pacifico')),
+          actions: <Widget>[
+            Switch(
+              thumbIcon: thumbIcon,
+              inactiveTrackColor: Colors.blue,
+              inactiveThumbColor: Colors.white,
+              value: mc.switchBool.value,
+              trackOutlineColor: outlineColor,
+              onChanged: (bool value) {
+                if (value) {
+                  mc.switchBool.value = true;
+                } else {
+                  mc.switchBool.value = false;
+                }
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            GestureDetector(
+              onTap: null,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                width: double.maxFinite,
+                height: 64,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                padding: const EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    const Icon(Icons.add_circle, size: 24),
+                    const SizedBox(width: 16),
+                    Text(
+                      mc.switchBool.value ? 'Add a movie' : 'Add a book',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
