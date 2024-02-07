be46bed1377076bac7798d84543275463d0d14cb
Anant Dubey (Intern) <intern.anant@sharekhan.com>
2024-02-07 11:57:58 +0530

    feat: add book and movie screens

---
+++ lib/screens/book.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BookScreen extends StatelessWidget {
+  const BookScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Welcome to Critiq books!',
+      ),
+    );
+  }
+}
