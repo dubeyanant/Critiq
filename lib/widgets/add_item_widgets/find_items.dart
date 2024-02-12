a93b42e8091036c429510086248f11cf407ea892
Anant Dubey (Intern) <intern.anant@sharekhan.com>
2024-02-12 17:35:59 +0530

    feat: implement movie api

--- lib/widgets/add_item_widgets/find_items.dart
+++ lib/widgets/add_item_widgets/find_items.dart
@@ -13,7 +13,7 @@ class FindItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final APIController bc = Get.put(APIController());
+    final APIController ac = Get.put(APIController());
     final SliderController sc = Get.put(SliderController());
     final ModeController mc = Get.put(ModeController());
     Timer? timer;
@@ -31,7 +31,7 @@ class FindItems extends StatelessWidget {
           onChanged: (value) {
             if (timer?.isActive ?? false) timer!.cancel();
             timer = Timer(const Duration(seconds: 1), () {
-              bc.itemName.value = value;
+              ac.itemName.value = value;
             });
           },
           textCapitalization: TextCapitalization.words,
@@ -40,9 +40,9 @@ class FindItems extends StatelessWidget {
         Expanded(
           child: Obx(() {
             return FutureBuilder(
-              future: bc.fetchBooks(),
+              future: mc.switchBool.value ? ac.fetchMovies() : ac.fetchBooks(),
               builder: (context, snapshot) {
-                if (bc.itemName.value == '') {
+                if (ac.itemName.value == '') {
                   return Center(
                     child: Text(
                       mc.switchBool.value
@@ -75,26 +75,34 @@ class FindItems extends StatelessWidget {
                       crossAxisSpacing: 16,
                       mainAxisSpacing: 16,
                     ),
-                    children: bc.apiDataList
-                        .where(
-                            (data) => data['volumeInfo']['imageLinks'] != null)
+                    children: ac.apiDataList
+                        .where((data) => mc.switchBool.value
+                            ? data['poster_path'] != null
+                            : data['volumeInfo']['imageLinks'] != null)
                         .map(
                           (data) => InkWell(
                             onTap: () {
                               aivc.sequenceCount++;
-                              sc.itemTitle.value =
-                                  data['volumeInfo']['title'].toString();
+                              sc.itemTitle.value = mc.switchBool.value
+                                  ? data['original_title'].toString()
+                                  : data['volumeInfo']['title'].toString();
                             },
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Expanded(
-                                  child: Image.network(data['volumeInfo']
-                                      ['imageLinks']['thumbnail']),
+                                  child: Image.network(
+                                    mc.switchBool.value
+                                        ? 'https://image.tmdb.org/t/p/original${data['poster_path']}'
+                                        : data['volumeInfo']['imageLinks']
+                                            ['thumbnail'],
+                                  ),
                                 ),
                                 const SizedBox(height: 16),
                                 Text(
-                                  data['volumeInfo']['title'].toString(),
+                                  mc.switchBool.value
+                                      ? data['original_title'].toString()
+                                      : data['volumeInfo']['title'].toString(),
                                   style: Theme.of(context).textTheme.bodyLarge,
                                   textAlign: TextAlign.center,
                                 ),
